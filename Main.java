9d6dd2d540ca877e7c5b55dc7283b017d0c9658c
Hadis <1hadis3.8moradi3@gmail.com>
2023-04-21 20:51:01 +0430

    Main function was edited

--- Main.java
+++ Main.java
@@ -1,9 +1,6 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to airline reservation system");
-        Menue.sign();
-
+        Menu.sign();
     }
 }
